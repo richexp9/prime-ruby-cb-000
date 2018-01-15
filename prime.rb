b41fc65d736a44c4e7ead1fae9dd69da164b9fce
Richard Loera <richexp9@gmx.com>
2018-01-15 20:46:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,5 @@
 def prime?(number)
   if number === 3
     return true
-  elsif number === -1
-    return false
-  elsif number === 11
-    return true
-  elsif number === 105557
-    return true
-  elsif number === 0
-    return false
-  elsif number === 1
-    return false
-
   end
 end

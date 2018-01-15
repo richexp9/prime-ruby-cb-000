992a9a973d2152a8bfe2a14844f6eb61b62ffa33
Richard Loera <richexp9@gmx.com>
2018-01-15 20:54:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,11 @@ def prime?(n)
          return true
       elsif n % 2 == 0 || n % 3 == 0
          return false
+      end
       i = 5
       while i * i <= n
          if n % i == 0 || n % (i + 2) == 0
              return false
          i = i + 6
       return true
-    end
 end

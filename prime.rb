3165d7cbccd57c56f006ff0896dc269d1e2dbfd0
Richard Loera <richexp9@gmx.com>
2018-01-15 20:54:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
-def prime?(number)
-  if number <= 1
-    return false
-
-  elsif number % number === 0
-    return true
-  else
-    return false
-  end
+def prime?(n)
+      if n <= 1
+         return false
+      elsif n <= 3
+         return true
+      elsif n % 2 == 0 || n % 3 == 0
+         return false
+      i = 5
+      while i * i <= n
+         if n % i == 0 || n % (i + 2) == 0
+             return false
+         i = i + 6
+      return true
 end
